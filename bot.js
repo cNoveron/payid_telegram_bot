e0fa5fdce7dcb4da2dff1ccfbdf08ab71314004b
carlosescom <carlos.escom.ipn@gmail.com>
2020-10-06 20:57:43 -0500

    bot: sends tx and then tries to validate it

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ bot.command('pay', (ctx) => {
     const sender = "r4ggLgv8MpvcqqkGj86TQWnkSYVSYSxhQb" // Satya's address
     console.log(amount, typeof amount)
 
+    let txID
     // const dropsAmount = api.xrpToDrops(BigNumber(amount).val)
     return api.preparePayment("r4ggLgv8MpvcqqkGj86TQWnkSYVSYSxhQb",{
         "source": {
@@ -41,8 +42,9 @@ bot.command('pay', (ctx) => {
 
         const sendersSecret = "sh9o5bCen9xRZ8cnqn8iYKffm2RDV" // Sathya's account secret
         const { id, signedTransaction } = api.sign(txJSON, sendersSecret)
-        console.log("Identifying hash:", id)
-        console.log("Signed blob:", signedTransaction)
+        txID = id
+        console.log("txID:", txID)
+        console.log("signedTransaction:", signedTransaction)
 
         return api.submit(signedTransaction)
         
@@ -55,12 +57,22 @@ bot.command('pay', (ctx) => {
         return api.getLedgerVersion()
 
     }).then((latestLedgerVersion) => {
-        console.log("Earliest ledger version:", latestLedgerVersion + 1)
 
+        const earliestLedgerVersion = latestLedgerVersion + 1
+        console.log("Earliest ledger version:", earliestLedgerVersion)
+
+        // earliestLedgerVersion was noted when the transaction was submitted.
+        // txID was noted when the transaction was signed.
+        return api.getTransaction(txID, { minLedgerVersion: earliestLedgerVersion })
+    
+    }).then((tx) => {
+
+        ctx.reply("Transaction result:", tx.outcome.result)
+        ctx.reply("Balance changes:", JSON.stringify(tx.outcome.balanceChanges))
+        ctx.reply(`${payID}`)
+        
     })
-        .catch((error) =>
-        console.log("Error data:", error.data)
-    )
+        .catch(console.log)
 
 
     // api.on('ledger', ledger => {
@@ -69,17 +81,6 @@ bot.command('pay', (ctx) => {
     //         ctx.reply("If the transaction hasn't succeeded by now, it's expired")
     //     }
     // })
-    
-    // // earliestLedgerVersion was noted when the transaction was submitted.
-    // // txID was noted when the transaction was signed.
-    // try {
-    //     const tx = await api.getTransaction(txID, { minLedgerVersion: earliestLedgerVersion })
-    //     ctx.reply("Transaction result:", tx.outcome.result)
-    //     ctx.reply("Balance changes:", JSON.stringify(tx.outcome.balanceChanges))
-    //     ctx.reply(`${payID}`)
-    // } catch (error) {
-    //     console.log("Couldn't get transaction outcome:", error)
-    // }
 
 })
 
