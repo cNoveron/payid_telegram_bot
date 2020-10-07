ebb387bc6e40fbc85ac8381076bbe23cebeddae4
carlosescom <carlos.escom.ipn@gmail.com>
2020-10-06 20:48:47 -0500

    bot: transction now succeeds

--- bot.js
+++ bot.js
@@ -10,55 +10,66 @@ bot.command('pay', (ctx) => {
 
     const inputs = ctx.message.text.split(' ')
     const payID = inputs[1]
-    const amount = inputs[2] + "000000"
+    const amount = inputs[2] //+ "000000"
     const sender = "r4ggLgv8MpvcqqkGj86TQWnkSYVSYSxhQb" // Satya's address
     console.log(amount, typeof amount)
 
     // const dropsAmount = api.xrpToDrops(BigNumber(amount).val)
-    api.prepareTransaction({
-        "TransactionType": "Payment",
-        "Account": sender,
-        "Amount": amount, // Same as "Amount": "22000000"
-        "Destination": "rMrudtZB5i2ks9rMMAMNS2cUixo8sfQyg1" // Shane's address
-    }, {
-        "maxLedgerVersionOffset": 75 // Expire this transaction if it doesn't execute within ~5 minutes:
+    return api.preparePayment("r4ggLgv8MpvcqqkGj86TQWnkSYVSYSxhQb",{
+        "source": {
+            "address": "r4ggLgv8MpvcqqkGj86TQWnkSYVSYSxhQb",
+            "maxAmount": {
+                "value": "1.000000",
+                "currency": "XRP"
+            } // Sathya's address
+        },
+        "destination": {
+            "address": "rMrudtZB5i2ks9rMMAMNS2cUixo8sfQyg1",
+            "amount": {
+                "value": "1.000000",
+                "currency": "XRP"
+            } // Shane's address
+        }
     }).then((preparedTx) => {
         
         const { txJSON, instructions } = preparedTx
         const { maxLedgerVersion, fee } = instructions
 
-        const txJSONstring = JSON.stringify(txJSON)
-        console.log("Prepared transaction instructions:", txJSONstring)
+        console.log("txJSON:", txJSON)
         console.log("Transaction cost:", fee, "XRP")
         console.log("Transaction expires after ledger:", maxLedgerVersion)
 
         const sendersSecret = "sh9o5bCen9xRZ8cnqn8iYKffm2RDV" // Sathya's account secret
-        const { id, signedTransaction } = api.sign(txJSONstring, sendersSecret)
+        const { id, signedTransaction } = api.sign(txJSON, sendersSecret)
         console.log("Identifying hash:", id)
         console.log("Signed blob:", signedTransaction)
 
-        return api.submit(txBlob)
+        return api.submit(signedTransaction)
         
     }).then((result) => {
 
         const { resultCode, resultMessage } = result
-        console.log("Tentative result code:", resultCode)
-        console.log("Tentative result message:", resultMessage)
+        console.log("resultCode:", resultCode)
+        console.log("resultMessage:", resultMessage)
+
+        return api.getLedgerVersion()
+
+    }).then((latestLedgerVersion) => {
+        console.log("Earliest ledger version:", latestLedgerVersion + 1)
 
     })
-    //     .catch((error) =>
-    //     console.log("Error data:", error.data)
-    // )
-    
-    // const latestLedgerVersion = (async () => await api.getLedgerVersion())()
-    // console.log("Earliest ledger version:", latestLedgerVersion + 1)
-    
+        .catch((error) =>
+        console.log("Error data:", error.data)
+    )
+
+
     // api.on('ledger', ledger => {
     //     console.log("Ledger version", ledger.ledgerVersion, "was validated.")
-    //     if (ledger.ledgerVersion > earliestLedgerVersion + 1) {
+    //     if (ledger.ledgerVersion > latestLedgerVersion + 1) {
     //         ctx.reply("If the transaction hasn't succeeded by now, it's expired")
     //     }
     // })
+    
     // // earliestLedgerVersion was noted when the transaction was submitted.
     // // txID was noted when the transaction was signed.
     // try {
@@ -71,5 +82,6 @@ bot.command('pay', (ctx) => {
     // }
 
 })
+
 
 bot.launch()
